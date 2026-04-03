573b4564e1706493545ae368066c3c8f52ea817d
Alieneer <37123222+BreavyTF2@users.noreply.github.com>
2026-04-02 22:56:04 -0400

    Documented the Animated tilesets

--- data/tileset_headers.asm
+++ data/tileset_headers.asm
@@ -5,37 +5,37 @@ SECTION "data/tileset_headers.asm", ROMX
 MACRO tileset
 	db BANK(\1_GFX) ; shared bank
 	dw \1_Meta, \1_GFX, \1_Coll
-	db \2, \3, \4 ; counter tiles
-	db \5         ; unknown
+	dw \2, ; animation set
+	dw NULL ; unused
 ENDM
 
 Tilesets:
-	; name, 3 counter tiles, unknown
-	tileset Tileset_00, $77, $40, $00, $00
-	tileset Tileset_01, $4b, $40, $00, $00
-	tileset Tileset_02, $1f, $40, $00, $00
-	tileset Tileset_03, $1f, $40, $00, $00
-	tileset Tileset_04, $4b, $40, $00, $00
-	tileset Tileset_05, $a7, $40, $00, $00
-	tileset Tileset_06, $4b, $40, $00, $00
-	tileset Tileset_07, $1f, $40, $00, $00
-	tileset Tileset_08, $4b, $40, $00, $00
-	tileset Tileset_09, $07, $41, $00, $00
-	tileset Tileset_0a, $07, $41, $00, $00
-	tileset Tileset_0b, $07, $41, $00, $00
-	tileset Tileset_0c, $07, $41, $00, $00
-	tileset Tileset_0d, $07, $41, $00, $00
-	tileset Tileset_0e, $07, $41, $00, $00
-	tileset Tileset_0f, $07, $41, $00, $00
-	tileset Tileset_10, $07, $41, $00, $00
-	tileset Tileset_11, $07, $41, $00, $00
-	tileset Tileset_12, $07, $41, $00, $00
-	tileset Tileset_13, $df, $40, $00, $00
-	tileset Tileset_14, $07, $41, $00, $00
-	tileset Tileset_15, $07, $41, $00, $00
-	tileset Tileset_16, $07, $41, $00, $00
-	tileset Tileset_17, $07, $41, $00, $00
-	tileset Tileset_18, $07, $41, $00, $00
-	tileset Tileset_19, $07, $41, $00, $00
-	tileset Tileset_1a, $07, $41, $00, $00
-	tileset Tileset_1b, $77, $40, $00, $00
+	; name, animation set, 2 unknown
+	tileset Tileset_00, TilesetGenericAnim, $00, $00
+	tileset Tileset_01, TilesetWaterAnim, $00, $00
+	tileset Tileset_02, TilesetFlowerAnim, $00, $00
+	tileset Tileset_03, TilesetFlowerAnim, $00, $00
+	tileset Tileset_04, TilesetWaterAnim, $00, $00
+	tileset Tileset_05, TilesetFontAnim, $00, $00
+	tileset Tileset_06, TilesetWaterAnim, $00, $00
+	tileset Tileset_07, TilesetFlowerAnim, $00, $00
+	tileset Tileset_08, TilesetWaterAnim, $00, $00
+	tileset Tileset_09, TilesetNoAnim, $00, $00
+	tileset Tileset_0a, TilesetNoAnim, $00, $00
+	tileset Tileset_0b, TilesetNoAnim, $00, $00
+	tileset Tileset_0c, TilesetNoAnim, $00, $00
+	tileset Tileset_0d, TilesetNoAnim, $00, $00
+	tileset Tileset_0e, TilesetNoAnim, $00, $00
+	tileset Tileset_0f, TilesetNoAnim, $00, $00
+	tileset Tileset_10, TilesetNoAnim, $00, $00
+	tileset Tileset_11, TilesetNoAnim, $00, $00
+	tileset Tileset_12, TilesetNoAnim, $00, $00
+	tileset Tileset_13, TilesetRocketHouseAnim, $00, $00
+	tileset Tileset_14, TilesetNoAnim, $00, $00
+	tileset Tileset_15, TilesetNoAnim, $00, $00
+	tileset Tileset_16, TilesetNoAnim, $00, $00
+	tileset Tileset_17, TilesetNoAnim, $00, $00
+	tileset Tileset_18, TilesetNoAnim, $00, $00
+	tileset Tileset_19, TilesetNoAnim, $00, $00
+	tileset Tileset_1a, TilesetNoAnim, $00, $00
+	tileset Tileset_1b, TilesetGenericAnim, $00, $00
